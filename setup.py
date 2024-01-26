cb7e9c2c46de89696b98b56bfd4d2e5910bd1772
Noemi Millman <noemi@triopter.com>
2024-01-26 16:20:05 -0500

    attempt at setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+setup(
+    name='mdx-escape-html',
+    version='0.1',
+    description='Python-markdown extension to escape HTML in raw markdown.',
+    long_description="",
+    author='Noemi Millman',
+    author_email='noemi@triopter.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["markdown"],
+    license="MIT",
+    zip_safe=False,
+)
